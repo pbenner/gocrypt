e75b8457c8abe3f475df6a80418e505ab8890724
Philipp Benner <philipp.benner@mis.mpg.de>
2016-06-29 11:03:33 +0200

    2016/06/29-11:03:33 (Linux thalamus unknown)

---
+++ polynomial.go
@@ -0,0 +1,299 @@
+/* Copyright (C) 2016 Philipp Benner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gocrypt
+
+/* -------------------------------------------------------------------------- */
+
+import "fmt"
+import "bufio"
+import "bytes"
+import "sort"
+
+/* -------------------------------------------------------------------------- */
+
+type Polynomial struct {
+  Terms map[int]float64
+}
+
+/* -------------------------------------------------------------------------- */
+
+func NewPolynomial() *Polynomial {
+  terms := make(map[int]float64)
+  return &Polynomial{terms}
+}
+
+/* -------------------------------------------------------------------------- */
+
+func (r *Polynomial) Clone() *Polynomial {
+  s := NewPolynomial()
+  for k, v := range r.Terms {
+    s.Terms[k] = v
+  }
+  return s
+}
+
+func (r *Polynomial) AddTerm(c float64, e int) {
+  r.Terms[e] += c
+  r.Clean()
+}
+
+func (r *Polynomial) Clear() {
+  r.Terms = make(map[int]float64)
+}
+
+func (r *Polynomial) Exponents() []int {
+  var keys []int
+  for k := range r.Terms {
+    keys = append(keys, k)
+  }
+  sort.Sort(sort.Reverse(sort.IntSlice(keys)))
+  return keys
+}
+
+func (r *Polynomial) Degree() int {
+  if len(r.Terms) == 0 {
+    return 0
+  }
+  return r.Exponents()[0]
+}
+
+func (r *Polynomial) Lead() (float64, int) {
+  if len(r.Terms) == 0 {
+    return 0, 0
+  }
+  k := r.Exponents()[0]
+
+  return r.Terms[k], k
+}
+
+func (r *Polynomial) Clean() {
+  for k, v := range r.Terms {
+    if v == 0 {
+      delete(r.Terms, k)
+    }
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+
+func (r *Polynomial) Equals(a *Polynomial) bool {
+  if r == a {
+    return true
+  } else {
+    if len(r.Terms) != len(a.Terms) {
+      return false
+    } else {
+      for k, v := range r.Terms {
+        if a.Terms[k] != v {
+          return false
+        }
+      }
+      return true
+    }
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+
+func (r *Polynomial) neg() {
+  for k, v := range r.Terms {
+    r.Terms[k] = -v
+  }
+}
+
+func (r *Polynomial) Neg(a *Polynomial) {
+  if r == a {
+    r.neg()
+  } else {
+    r.Clear()
+    r.add(a)
+    r.neg()
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+
+func (r *Polynomial) add(a *Polynomial) {
+  for k, v := range a.Terms {
+    r.Terms[k] += v
+  }
+  r.Clean()
+}
+
+func (r *Polynomial) Add(a, b *Polynomial) {
+  if r == a {
+    r.add(b)
+  } else if r == b {
+    r.add(a)
+  } else {
+    r.Clear()
+    r.add(a)
+    r.add(b)
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+
+func (r *Polynomial) sub(a *Polynomial) {
+  for k, v := range a.Terms {
+    r.Terms[k] -= v
+  }
+  r.Clean()
+}
+
+func (r *Polynomial) Sub(a, b *Polynomial) {
+  if r == a {
+    r.sub(b)
+  } else if r == b {
+    r.neg()
+    r.add(a)
+  } else {
+    r.Clear()
+    r.add(a)
+    r.sub(b)
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+
+func (r *Polynomial) mul(a *Polynomial) {
+  t := NewPolynomial()
+  for k1, v1 := range r.Terms {
+    for k2, v2 := range a.Terms {
+      k := k1+k2
+      v := v1*v2
+      t.Terms[k] += v
+    }
+  }
+  r.Terms = t.Terms
+  r.Clean()
+}
+
+func (r *Polynomial) Mul(a, b *Polynomial) {
+  if r == a || r == b {
+    r.mul(b)
+  } else {
+    r.Clear()
+    r.add(a)
+    r.mul(b)
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+
+func (s *Polynomial) Div(a, b *Polynomial) *Polynomial {
+  z := NewPolynomial()
+  t := NewPolynomial()
+  q := NewPolynomial()
+  r := a.Clone()
+  if b.Equals(z) {
+    panic("Div(): division by zero")
+  }
+  c2, e2 := b.Lead()
+  for !r.Equals(z) && r.Degree() >= b.Degree() {
+    c1, e1 := r.Lead()
+    t.Clear()
+    t.AddTerm(c1/c2, e1-e2)
+    q.AddTerm(c1/c2, e1-e2)
+    t.Mul(t, b)
+    r.Sub(r, t)
+  }
+  s.Terms = q.Terms
+
+  return r
+}
+
+/* -------------------------------------------------------------------------- */
+
+func (r *Polynomial) Mod(a, b *Polynomial) {
+  s := r.Div(a, b)
+  r.Terms = s.Terms
+}
+
+/* -------------------------------------------------------------------------- */
+
+func (ri *Polynomial) EEA(rj *Polynomial) (*Polynomial, *Polynomial, *Polynomial) {
+
+  z0 := NewPolynomial()
+  si := NewPolynomial()
+  si.AddTerm(1, 0)
+  ti := NewPolynomial()
+  // j = i+1
+  sj := NewPolynomial()
+  tj := NewPolynomial()
+  tj.AddTerm(1, 0)
+  qj := NewPolynomial()
+  // k = j+1
+  sk := NewPolynomial()
+  tk := NewPolynomial()
+  rk := NewPolynomial()
+
+  for !rj.Equals(z0) {
+    // r_i = r_i-2 mod r_i-1
+    rk.Mod(ri, rj)
+    // q_i-1 = (r_i-2 - r_i)/r_i-1
+    qj.Sub(ri, rk)
+    qj.Div(qj, rj)
+    // s_i = s_i-2 - q_i-1*s_i-1  
+    sk.Mul(qj, sj)
+    sk.Sub(si, sk)
+    // t_i = t_i-2 - q_i-1*t_i-1
+    tk.Mul(qj, tj)
+    tk.Sub(ti, tk)
+
+    si, sj, sk = sj, sk, si
+    ti, tj, tk = tj, tk, ti
+    ri, rj, rk = rj, rk, ri
+  }
+  if _, e := ri.Lead(); e == 0 {
+    si.Div(si, ri)
+    ti.Div(ti, ri)
+    ri.Div(ri, ri)
+  }
+  // gcd(r0, r1) = ri = s r_0 + t r_1
+  return ri, si, ti
+}
+
+/* -------------------------------------------------------------------------- */
+
+func (r *Polynomial) String() string {
+
+  var buffer bytes.Buffer
+  writer := bufio.NewWriter(&buffer)
+
+  keys := r.Exponents()
+
+  for i, k := range keys {
+    v := r.Terms[k]
+    if i != 0 {
+      if v >= 0.0 {
+        fmt.Fprintf(writer, " + ")
+      } else {
+        fmt.Fprintf(writer, " - ")
+        v = -v
+      }
+    }
+    if k == 0 {
+      fmt.Fprintf(writer, "%f", v)
+    } else {
+      fmt.Fprintf(writer, "%fx^%d", v, k)
+    }
+  }
+  writer.Flush()
+
+  return buffer.String()
+}
---
+++ polynomial_test.go
@@ -0,0 +1,212 @@
+/* Copyright (C) 2016 Philipp Benner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gocrypt
+
+/* -------------------------------------------------------------------------- */
+
+//import "fmt"
+import "math"
+import "testing"
+
+/* -------------------------------------------------------------------------- */
+
+func TestPolynomial1(t *testing.T) {
+
+  p := NewPolynomial()
+  p.AddTerm(20,30)
+  p.AddTerm(2,3)
+  q := p.Clone()
+  q.AddTerm(1024, 256)
+
+  r := NewPolynomial()
+  r.Mul(p,q)
+  // 20480x^286 + 2048x^259 + 400x^60 + 80x^33 + 4x^6
+  s := NewPolynomial()
+  s.AddTerm(20480, 286)
+  s.AddTerm( 2048, 259)
+  s.AddTerm(  400,  60)
+  s.AddTerm(   80,  33)
+  s.AddTerm(    4,   6)
+
+  if !s.Equals(r) {
+    t.Error("polynomial test failed")
+  }
+
+}
+
+func TestPolynomial2(t *testing.T) {
+
+  p := NewPolynomial()
+  p.AddTerm(20, 30)
+  p.AddTerm( 2  ,3)
+  q := p.Clone()
+
+  r1 := NewPolynomial()
+  r1.Add(p, q)
+
+  r2 := NewPolynomial()
+  r2.Add(p, p)
+
+  r3 := p.Clone()
+  r3.Add(r3, p)
+
+  r4 := p.Clone()
+  r4.Add(p, r4)
+
+  r5 := p.Clone()
+  r5.Add(r5, r5)
+
+  if !r1.Equals(r2) {
+    t.Error("polynomial test failed")
+  }
+  if !r1.Equals(r3) {
+    t.Error("polynomial test failed")
+  }
+  if !r1.Equals(r4) {
+    t.Error("polynomial test failed")
+  }
+  if !r1.Equals(r5) {
+    t.Error("polynomial test failed")
+  }
+
+}
+
+func TestPolynomial3(t *testing.T) {
+
+  p := NewPolynomial()
+  p.AddTerm(20, 30)
+  p.AddTerm( 2  ,3)
+  q := p.Clone()
+  p.AddTerm(10, 35)
+
+  r1 := NewPolynomial()
+  r1.Sub(p, q)
+
+  r2 := p.Clone()
+  r2.Sub(r2, q)
+
+  r3 := q.Clone()
+  r3.Sub(p, r3)
+
+  r4 := p.Clone()
+  r4.Sub(r4, r4)
+
+  if !r1.Equals(r2) {
+    t.Error("polynomial test failed")
+  }
+  if !r1.Equals(r3) {
+    t.Error("polynomial test failed")
+  }
+  if !r4.Equals(NewPolynomial()) {
+    t.Error("polynomial test failed")
+  }
+
+}
+
+func TestPolynomial4(t *testing.T) {
+
+  p := NewPolynomial()
+  p.AddTerm(20, 30)
+  p.AddTerm( 2  ,3)
+  q := p.Clone()
+
+  r1 := NewPolynomial()
+  r1.Mul(p, q)
+
+  r2 := NewPolynomial()
+  r2.Mul(p, p)
+
+  r3 := p.Clone()
+  r3.Mul(r3, p)
+
+  r4 := p.Clone()
+  r4.Mul(p, r4)
+
+  r5 := p.Clone()
+  r5.Mul(r5, r5)
+
+  if !r1.Equals(r2) {
+    t.Error("polynomial test failed")
+  }
+  if !r1.Equals(r3) {
+    t.Error("polynomial test failed")
+  }
+  if !r1.Equals(r4) {
+    t.Error("polynomial test failed")
+  }
+  if !r1.Equals(r5) {
+    t.Error("polynomial test failed")
+  }
+
+}
+
+func TestPolynomial5(t *testing.T) {
+
+  p := NewPolynomial()
+  p.AddTerm( 1, 3)
+  p.AddTerm(-2, 2)
+  p.AddTerm(-4, 0)
+  q := NewPolynomial()
+  q.AddTerm( 1, 1)
+  q.AddTerm(-3, 0)
+
+  r1 := NewPolynomial()
+  r1.AddTerm( 1, 2)
+  r1.AddTerm( 1, 1)
+  r1.AddTerm( 3, 0)
+
+  r2 := NewPolynomial()
+  r2.AddTerm( 5, 0)
+
+  r3 := NewPolynomial()
+  r4 := r3.Div(p, q)
+
+  if !r3.Equals(r1) {
+    t.Error("polynomial test failed")
+  }
+  if !r4.Equals(r2) {
+    t.Error("polynomial test failed")
+  }
+
+}
+
+func TestPolynomial6(t *testing.T) {
+
+  p := NewPolynomial()
+  p.AddTerm( 1, 8)
+  p.AddTerm( 1, 4)
+  p.AddTerm( 1, 3)
+  p.AddTerm( 1, 1)
+  p.AddTerm( 1, 0)
+  a := NewPolynomial()
+  a.AddTerm( 1, 7)
+  a.AddTerm( 1, 6)
+  a.AddTerm( 1, 1)
+
+  r, si, ti := a.EEA(p)
+
+  if c, e := r.Lead(); math.Abs(c - 1.0) > 1e-12 || e != 0 {
+    t.Error("polynomial test failed")
+  }
+  if c, e := si.Lead(); math.Abs(c - -0.796748) > 1e-4 || e != 7 {
+    t.Error("polynomial test failed")
+  }
+  if c, e := ti.Lead(); math.Abs(c - 0.796748) > 1e-4 || e != 6 {
+    t.Error("polynomial test failed")
+  }
+
+}
